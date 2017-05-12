50f774845bff184b80487ceb5fa6195ba3ab79f0
Matt <matty_noye@live.com>
2017-05-12 03:24:28 +0100

    Basic layout added to login
    The basic layout has been added to the login page.  More work to come

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="robots" content="all,follow">
+            <meta name="googlebot" content="index,follow,snippet,archive">
+            
+            <!-- link the connector stylsheet -->
+            <link rel="stylesheet" href="style.css">
+            
+            <!-- link google font -->
+            <link href="https://fonts.googleapis.com/css?family=Yrsa" rel="stylesheet">
+            
+            <title>Login</title>
+	</head>
+	<body>
+            <section id="login-main">
+                
+            </section>
+	</body>
+</html>
