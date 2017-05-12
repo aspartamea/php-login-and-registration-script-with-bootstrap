f5b26dc202ac4a1a4d3d88db256fd42a6d012cba
Matt <matty_noye@live.com>
2017-05-12 03:51:17 +0100

    Login form created
    Login form created on the login page

--- login.php
+++ login.php
@@ -13,11 +13,49 @@
             <!-- link google font -->
             <link href="https://fonts.googleapis.com/css?family=Yrsa" rel="stylesheet">
             
-            <title>Login</title>
+            <title>Login | User authenication system</title>
 	</head>
 	<body>
-            <section id="login-main">
-                
-            </section>
+        <section id="login-main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
+                        <h1>User Authentication system.</h1>
+                        <br>
+                            
+                        <div id="usr">
+                            <p>
+                                <a href="index.php">Back</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <form class="form-horizontal" id="lo-form" action="post" method="">
+                            <div class="form-group col-md-12">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-user"></i>
+                                    </span>
+                                    <input class="form-control" type="text" value="" placeholder="username" name="username" required>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-key"></i>
+                                    </span>
+                                    <input class="form-control" type="password" value="" placeholder="password" name="password" required>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-12 button text-center">
+                                <button name="submit" type="submit" class="btn btn-primary btn-lg" value="Singin">
+                                    Login 
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
 	</body>
 </html>
