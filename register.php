d3decff587ff1efffe63e4547d15c494675b7da2
Matt <matty_noye@live.com>
2017-05-14 00:34:45 +0100

    Re upload of files

--- register.php
+++ register.php
@@ -1,46 +1,88 @@
 <?php 
-    
+    //Add the db con
     include_once'config/Database.php';
 
-    if(isset($_POST['email'])) {
-        $email = $_POST['email'];
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        
-        //Hash PWD
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        
+    //process the form 
+    if(isset($_POST['registerBtn'])){
+        //Initialise the array to store messages from form
+        $form_errors = array();
 
-        $sqlInsert = "INSERT INTO users (username, email, password, join_date)
-                    VALUES (:username, :email, :password, now())";
+        //form validation
+        $required_fields = array('email', 'username', 'password');
 
-        try {
-            $statement = $db->prepare($sqlInsert);
-            $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
-
-            if($statement->rowCount() == 1) {
-                $result = '
-                            <div class="alert alert-success" role="alert">
-                                Registration successful.<br>
-                            </div>
-                        ';
+        //loop through the required fields 
+        foreach($required_fields as $name_of_field){
+            if(!isset($_POST[$name_of_field]) || $_POST[$name_of_field] == NULL){
+                $form_errors[] = $name_of_field . " is a required field";
             }
         }
-        catch(PDOException $ex) {
-            $result = '
-                        <div class="alert alert-warning" role="alert">
-                            An error has occured: '.$ex->getMessage().'
+
+        //check if error array is empty, if yes process and insert data
+        if(empty($form_errors)){
+            //Collect form data and store in vars
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            //Hash the pwd
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            try{
+                //Create SQL insert 
+                $sqlInsert = "INSERT INTO users (username, email, password, join_date)
+                                VALUES (:username, :email, :password, now())";
+
+                //sanitise data
+                $statement = $db->prepare($sqlInsert);
+
+                //Add data into the db 
+                $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+
+                //Check if one new row has been created 
+                if($statement->rowCount() == 1){
+                    $result = '
+                        <div class="alert alert-success">
+                            Registration successful!
                         </div>
                     ';
+                }
+            }
+            catch(PDOException $ex) {
+                $result = '
+                    <div class="alert alert-warning">
+                        An error occured: '.$ex->getMessage().'
+                    </div>
+                ';
+            }
+        }
+        else {
+            if(count($form_errors) == 1){
+                $result = '<div class="alert alert-warning">There was 1 error in the form<br>';
+
+                $result .= '<ul>';
+                //loop through array and display all items 
+                foreach($form_errors as $error){
+                    $result .= '<li> {$error} </li>';
+                }
+                $result .= '</ul></div>';
+            }
+            else {
+                $result = '<div class="alert alert-warning"> There were ' .count($form_errors). 'errors in the form<br>';
+                $result .= '<ul>';
+                //loop through array and display all items 
+                foreach($form_errors as $error){
+                    $result .= '<li> {$error} </li>';
+                }
+                $result .= '</ul></div>';
+            }
         }
     }
-
+   
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head lang="en">
-            <meta charset="utf-8">
+            <!--<meta charset="utf-8"> -->
             <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <meta name="robots" content="all,follow">
@@ -82,7 +124,7 @@
                                     <span class="input-group-addon">
                                         <i class="fa fa-envelope"></i>
                                     </span>
-                                    <input class="form-control" type="text" value="" placeholder="Your email address" name="email">
+                                    <input class="form-control" type="text" placeholder="Your email address" name="email" required>
                                 </div>
                             </div>
                             <div class="form-group col-md-12">
@@ -90,7 +132,7 @@
                                     <span class="input-group-addon">
                                         <i class="fa fa-user"></i>
                                     </span>
-                                    <input class="form-control" type="text" value="" placeholder="username" name="username" required>
+                                    <input class="form-control" type="text" placeholder="username" name="username" required>
                                 </div>
                             </div>
                             <div class="form-group col-md-12">
@@ -98,13 +140,12 @@
                                     <span class="input-group-addon">
                                         <i class="fa fa-key"></i>
                                     </span>
-                                    <input class="form-control" type="password" value="" placeholder="password" name="password" required>
+                                    <input class="form-control" type="password" placeholder="password" name="password" required>
                                 </div>
                             </div>
                             <div class="form-group col-md-12 button text-center">
-                                <button name="submit" type="submit" class="btn btn-primary btn-lg" value="Singin">
-                                    Register 
-                                </button>
+                                <input name="registerBtn" type="submit" value="Register" class="btn btn-primary btn-lg">
+                                    
                             </div>
                         </form>
                     </div>
