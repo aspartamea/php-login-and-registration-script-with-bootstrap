e4b12e3b4a345402b15699b889e93b855c876bdb
Matt <matty_noye@live.com>
2017-05-14 23:00:18 +0100

    Login script finished
    Finished working on the login script.  Now is fully working

--- login.php
+++ login.php
@@ -1,3 +1,59 @@
+<?php 
+    include_once'config/Session.php';
+    include_once'config/Database.php';
+    include_once'config/Utilities.php';
+
+    if(isset($_POST['loginBtn'])){
+        //initialise the array 
+        $form_errors = array();
+
+        //Validate the form
+        $required_fields = array('username', 'password');
+
+        $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
+
+        if(empty($form_errors)){
+            //Collect the form data
+            $user = $_POST['username'];
+            $password = $_POST['password'];
+
+            //check if user exists in the db 
+            $sqlQuery = "SELECT * FROM users WHERE username = :username";
+            $statement = $db->prepare($sqlQuery);
+            $statement->execute(array(':username' => $user));
+
+            //fetch data from DB & compare it with inputted data 
+            while($row = $statement->fetch()){
+                $id = $row['id'];
+                $hashed_password = $row['password'];
+                $username = $row['username'];
+
+                //If pwd's match create the session 
+                if(password_verify($password, $hashed_password)){
+                    $_SESSION['id'] = $id;
+                    $_SESSION['username'] = $username;
+                    //Change below here to where you want users to be redirected upon login
+                    header("location: index.php");
+                }
+                else{
+                    $result = '<div class="alert alert-danger" role="alert"> Invalid username or password!</div>';
+                }
+            }
+
+
+        }
+        else {
+            if(count($form_errors) == 1) {
+                $result = '<div class="alert alert-danger" role="alert">There was 1 error in the form<br>';
+            }
+            else {
+                $result = '<div class="alert alert-danger" role="alert">There were ' .count($form_errors). ' errors in the form<br>'; 
+            }
+        }
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html>
 	<head lang="en">
@@ -22,21 +78,22 @@
                     <div class="col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
                         <h1>User Authentication system.</h1>
                         <br>
-                            
-                        <div id="usr">
-                            <p>
-                                <a href="index.php">Back</a>
-                            </p>
-                        </div>
+
+                        <?php if(isset($result)){
+                            echo $result;
+                        } ?>
+                        <?php if(!empty($form_errors)){
+                            echo show_errors($form_errors);
+                        } ?>
                     </div>
                     <div class="col-md-6 col-md-offset-3 text-center">
-                        <form class="form-horizontal" id="lo-form" action="post" method="">
+                        <form class="form-horizontal" action="" method="POST">
                             <div class="form-group col-md-12">
                                 <div class="input-group">
                                     <span class="input-group-addon">
                                         <i class="fa fa-user"></i>
                                     </span>
-                                    <input class="form-control" type="text" value="" placeholder="username" name="username" required>
+                                    <input class="form-control" type="text" placeholder="username" name="username">
                                 </div>
                             </div>
                             <div class="form-group col-md-12">
@@ -44,15 +101,20 @@
                                     <span class="input-group-addon">
                                         <i class="fa fa-key"></i>
                                     </span>
-                                    <input class="form-control" type="password" value="" placeholder="password" name="password" required>
+                                    <input class="form-control" type="password" placeholder="password" name="password">
                                 </div>
                             </div>
                             <div class="form-group col-md-12 button text-center">
-                                <button name="submit" type="submit" class="btn btn-primary btn-lg" value="Singin">
-                                    Login 
-                                </button>
+                                <input name="loginBtn" type="submit" class="btn btn-primary btn-lg" value="Login">
                             </div>
                         </form>
+                    </div>
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <div id="usr">
+                            <p>
+                                <a href="index.php">Back</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
