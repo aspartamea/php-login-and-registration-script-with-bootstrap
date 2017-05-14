278b88030b7e2ed24eeb567f00603f8b04496e23
Matt <matty_noye@live.com>
2017-05-14 23:20:04 +0100

    Better logout features added
    I have now created a better logout script.  This script will now destroy any cookies stored when loggin in and also fully destroy the session.

--- logout.php
+++ logout.php
@@ -1,6 +1,19 @@
 <?php 
 	include_once'config/Session.php';
 
+	//Unset all of the session vars
+	$_SESSION = array(); 
+
+	//This will kill the session & destroy any cookies
+	if (ini_get("session.use_cookies")){
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', time() -42000,
+			$params["path"], $params["domain"],
+			$params["secure"], $params["httponly"]
+		);
+	}
+
+	//Finally destroy the session
 	session_destroy();
 
 	header('location: index.php');
