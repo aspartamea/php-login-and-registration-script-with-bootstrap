41958f8f8a8be926093897adb7c9d0d4a2a9bd3a
Matt <matty_noye@live.com>
2017-06-02 04:19:30 +0100

    Changes to the login form
    I have made some changes to the look of the form,  this is to incorporate the remember me button better that previously.

--- login.php
+++ login.php
@@ -7,8 +7,8 @@
         <section id="login-main">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8 col-md-offset-2 col-sm-12 col-xs-12">
-                        <h1>User Authentication system.</h1>
+                    <h1>User Authentication system.</h1>
+                    <div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
                         <h3>Login</h3>
                         <br>
 
@@ -19,7 +19,7 @@
                             echo show_errors($form_errors);
                         } ?>
                     </div>
-                    <div class="col-md-6 col-md-offset-3 text-center">
+                    <div class="col-md-4 col-md-offset-4 text-center">
                         <form class="form-horizontal" action="" method="POST">
                             <div class="form-group col-md-12">
                                 <div class="input-group">
@@ -37,11 +37,18 @@
                                     <input class="form-control" type="password" placeholder="password" name="password">
                                 </div>
                             </div>
-                            <div class="form-group col-md-12 button text-center">
+                            <div class="form-group col-md-6">
+                                <div class="remember-me">
+                                    <label><input name="remember" value="yes" type="checkbox">Remember Me</label>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6 button">
                                 <input name="loginBtn" type="submit" class="btn btn-primary btn-lg pull-right" value="Login"><br />
-                                <a href="forgot_pwd.php">Forgotten password?</a> <br /> <label><input name="remember" type="checkbox">Remember Me</label>
                             </div>
                         </form>
+                        <div class="col-md-12 text-center lost-pwd">
+                            <a href="forgot_pwd.php">Lost your password?</a>
+                        </div>
                     </div>
                 </div>
             </div>
